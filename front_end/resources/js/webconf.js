931971b5b5852a8d88b71a7c14305b4fe7f11b96
NJSobral <joaosobral15pt@gmail.com>
2019-08-08 16:32:48 +0100

    initial script and css for header

---
+++ front_end/resources/js/webconf.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+    //codigo para a manipulacao DOM
+}
+
+const btnRegister = document.getElementById("btnRegister")
+btnRegister.addEventListener("click", function(){})
